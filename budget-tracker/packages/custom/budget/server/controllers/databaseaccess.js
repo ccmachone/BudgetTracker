199d0de76f38fd690a314cb3bac21510fbbf2ea0
Cameron Hartmann <CamHart73@gmail.com>
2015-02-05 22:27:46 -0700

    database endpoints added for debugging

---
+++ budget-tracker/packages/custom/budget/server/controllers/databaseaccess.js
@@ -0,0 +1,45 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+	Transaction = mongoose.model('Transaction'),
+	User = mongoose.model('User'),
+	Profile = mongoose.model('Profile'),
+	Budget = mongoose.model('Budget'),
+	Envelope = mongoose.model('Envelope'),
+	Login = mongoose.model('Login');
+
+
+var databaseRetriever = function(SchemaModel, req, res, next) {
+	SchemaModel.find(function(err, objs) {
+		res.status(200);
+		if(err) {
+			res.send('Something went wrong with the database lookup');
+		} else {
+			res.send(objs);
+		}
+	});
+};
+
+exports.getTransactions = function(req, res, next) {
+	databaseRetriever(Transaction, req, res, next);
+};
+
+exports.getUsers = function(req, res, next) {
+	databaseRetriever(User, req, res, next);
+};
+
+exports.getProfiles = function(req, res, next) {
+	databaseRetriever(Profile, req, res, next);
+};
+
+exports.getBudgets = function(req, res, next) {
+	databaseRetriever(Budget, req, res, next);
+};
+
+exports.getEnvelopes = function(req, res, next) {
+	databaseRetriever(Envelope, req, res, next);
+};
+
+exports.getLogins = function(req, res, next) {
+	databaseRetriever(Login, req, res, next);
+};
--- budget-tracker/packages/custom/budget/server/routes/budget.js
+++ budget-tracker/packages/custom/budget/server/routes/budget.js
@@ -3,6 +3,7 @@
 /* jshint -W098 */
 
 var transaction = require('../controllers/transaction'),
+    databaseAccess = require('../controllers/databaseaccess'),
     config = require('meanio').loadConfig();
 
 // The Package is past automatically as first parameter
@@ -32,5 +33,34 @@ module.exports = function(Budget, app, auth, database) {
 // Create a new transaction
     app.post('/transaction/create', function(req, res, next) {
         transaction.create(req,res,next);
+    });
+
+
+
+
+// Database routes
+
+    app.get('/database/transactions', function(req, res, next) {
+        databaseAccess.getTransactions(req, res, next);
+    });
+
+    app.get('/database/users', function(req, res, next) {
+        databaseAccess.getUsers(req, res, next);
+    });
+
+    app.get('/database/profiles', function(req, res, next) {
+        databaseAccess.getProfiles(req, res, next);
+    });
+
+    app.get('/database/budgets', function(req, res, next) {
+        databaseAccess.getBudgets(req, res, next);
+    });
+
+    app.get('/database/envelopes', function(req, res, next) {
+        databaseAccess.getEnvelopes(req, res, next);
+    });
+
+    app.get('/database/logins', function(req, res, next) {
+        databaseAccess.getLogins(req, res, next);
     });
 };

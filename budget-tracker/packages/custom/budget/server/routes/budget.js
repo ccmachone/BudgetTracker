27de5608873c66a208c4213de3264deadfb8de3f
Nick Martin <nicholas.martin90@gmail.com>
2015-02-10 20:06:44 -0700

    Adding unit tests

--- budget-tracker/packages/custom/budget/server/routes/budget.js
+++ budget-tracker/packages/custom/budget/server/routes/budget.js
@@ -9,35 +9,7 @@ var transactionController = require('../controllers/transaction'),
     passport = require('passport'),
     config = require('meanio').loadConfig();
 
-// The Package is past automatically as first parameter
 module.exports = function(Budget, app, auth, database) {
-
-    // app.post('/login', passport.authenticate('local', {
-    //         successRedirect: '/',
-    //         failureRedirect: '/login'
-    //     })
-    // );
-
-    app.get('/budget/example/anyone', function(req, res, next) {
-        res.send('Anyone can access this');
-    });
-
-    app.get('/budget/example/auth', auth.requiresLogin, function(req, res, next) {
-        res.send('Only authenticated users can access this');
-    });
-
-    app.get('/budget/example/admin', auth.requiresAdmin, function(req, res, next) {
-        res.send('Only users with Admin role can access this');
-    });
-
-    app.get('/budget/example/render', function(req, res, next) {
-        Budget.render('index', {
-            package: 'budget'
-        }, function(err, html) {
-            //Rendering a view from the Package server/views
-            res.send(html);
-        });
-    });
 
 // TRANSACTION
     

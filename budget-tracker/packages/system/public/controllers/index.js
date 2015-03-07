5458b7cd404db4e0530e32afbb7dfdbd8c3f9546
Jake Parkinson <parkinson9@gmail.com>
2015-03-07 15:15:04 -0700

    Add envelope button

--- budget-tracker/packages/system/public/controllers/index.js
+++ budget-tracker/packages/system/public/controllers/index.js
@@ -4,6 +4,9 @@ angular.module('mean.system').controller('IndexController', ['$scope', 'Global',
   function($scope, Global) {
     $scope.global = Global;
     
+    $scope.addEnvelope = function(){
+      console.log("Add envelope");
+    }
 
    
   }

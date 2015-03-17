067359e61dde5714a1fcd69bb36d6625b4ebede0
nikolashin <g.nikolashin@gmail.com>
2015-03-17 11:31:35 -0600

    improved accordian menu on envelope page with some basic input checks and added an amount field

--- budget-tracker/packages/system/public/controllers/index.js
+++ budget-tracker/packages/system/public/controllers/index.js
@@ -5,7 +5,18 @@ angular.module('mean.system').controller('IndexController', ['$scope', 'Global',
     $scope.global = Global;
     
     $scope.addEnvelope = function(){
+		
+		if(this.envname == undefined || this.envamt == undefined ||
+			this.envname == '' || this.envamt == '' )
+		{
+			alert('One or more fields empty');
+			return;
+		}
       console.log(this.envname);
+	  console.log(this.envamt);
+	  
+	  this.envname = '';
+	  this.envamt = '';
     };
 	
 	$scope.oneAtATime = true;

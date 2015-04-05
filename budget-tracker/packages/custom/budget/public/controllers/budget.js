d99172b461ae82a78961feac385dbab693cf7df5
nikolashin <g.nikolashin@gmail.com>
2015-04-05 02:32:06 -0600

    Update to Account page, changes not persistent. Made minor change to server index controller.

--- budget-tracker/packages/custom/budget/public/controllers/budget.js
+++ budget-tracker/packages/custom/budget/public/controllers/budget.js
@@ -8,7 +8,36 @@ angular.module('mean.budget').controller('BudgetController', ['$scope', 'Global'
       name: 'budget'
     };
 	
+	$scope.fullname = Global.user.name;
 	$scope.username = Global.user.username;
+	$scope.email = Global.user.email;
+	
+	$scope.noEdit = false;
+	$scope.edit = true;
+	
+	$scope.editAccount = function(){
+		this.noEdit = true;
+		this.edit = false;
+		
+		this.newName = this.fullname;
+		this.newUsername = this.username;
+		this.newEmail = this.email;
+	};
+	
+	$scope.updateUser = function(){
+		this.noEdit = false;
+		this.edit = true;
+
+		this.fullname = this.newName;
+		this.username = this.newUsername;
+		this.email = this.newEmail;
+	};
+	
+	$scope.cancelUpdate = function(){
+		this.noEdit = false;
+		this.edit = true;	
+	};
+	
 	console.log(Global.user);
   }
 ]);
